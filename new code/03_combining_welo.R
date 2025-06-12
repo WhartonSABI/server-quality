eb8dcdc8e041faa601812a9e8c5937cf2d6d9b80
aiwenli123 <ali52@hawk.iit.edu>
2025-06-12 15:28:33 -0400

    resort files, and getting 2024 welo m and f (wimbledon)

--- new code/03_combining_welo.R
+++ new code/03_combining_welo.R
@@ -1,23 +1,8 @@
 # rm(list=ls())
 # install.packages("welo")
 library(welo)
-
-#-----------------------------------------------------------------------------------------------------
-
-wta_2021 <- tennis_data("2021", "WTA") # female = wta
-
-atp_2021 <- tennis_data("2021", "ATP") # male = atp
-atp_2021_clean <- clean(atp_2021)
-
-atp_2021_welo <- welofit(atp_2021_clean)$results
-
-#-----------------------------------------------------------------------------------------------------
-
-# testing on 2024 data
-atp_2024 <- tennis_data("2024", "ATP")
-atp_2024_clean <- clean(atp_2024)
-
-atp_2024_welo <- welofit(atp_2024_clean)$results
+library(tidyverse)
+library(data.table)
 
 #-----------------------------------------------------------------------------------------------------
 
@@ -43,7 +28,7 @@ add_speed_ratio_column <- function(data) {
 
 #-----------------------------------------------------------------------------------------------------
 
-# wimbledon 2024 from github:
+### 2024 data
 
 # binary variable for whether the point was won by the server
 wimbledon_2024 <- as.data.table(read.csv("../data/wimbledon_2024_combined.csv"))
@@ -60,16 +45,14 @@ wimbledon_2024_male <- add_speed_ratio_column(wimbledon_2024_male)
 wimbledon_2024_female <- add_speed_ratio_column(wimbledon_2024_female)
 
 subset_2024_m <- wimbledon_2024_male %>% # to make it easier for us to look through relevant columns
-  select(match_id, slam, year, player1, player2, Speed_MPH, ServeNumber, PointServer, PointWinner, ServeWidth, ServeDepth, RallyCount, GameNo, P1DistanceRun,
+  select(match_id, slam, year, PointNumber, player1, player2, Speed_MPH, ServeNumber, PointServer, PointWinner, ServeWidth, ServeDepth, RallyCount, GameNo, P1DistanceRun,
          P2DistanceRun, RallyCount, serving_player_won, speed_ratio)
 
 subset_2024_f <- wimbledon_2024_female %>% # to make it easier for us to look through relevant columns
-  select(match_id, slam, year, player1, player2, Speed_MPH, ServeNumber, PointServer, PointWinner, ServeWidth, ServeDepth, RallyCount, GameNo, P1DistanceRun,
+  select(match_id, slam, year, PointNumber, player1, player2, Speed_MPH, ServeNumber, PointServer, PointWinner, ServeWidth, ServeDepth, RallyCount, GameNo, P1DistanceRun,
          P2DistanceRun, RallyCount, serving_player_won, speed_ratio)
 
-#-----------------------------------------------------------------------------------------------------
-
-# add new column in subset_2024_m for player1_name being last name (which includes every word in player1 except for 
+# add new column for player1_name being last name (which includes every word in player1 except for 
 # the first word) followed by their first initial, then a period.
 
 subset_2024_m <- subset_2024_m %>%
@@ -86,13 +69,30 @@ subset_2024_m <- subset_2024_m %>%
   ) %>%
   ungroup()
 
-# in atp_2021_welo, select only rows where the entry in "Series" is "Grand Slam" and only where
-# the entry in "Surface" is "Grass"
+subset_2024_f <- subset_2024_f %>%
+  rowwise() %>%
+  mutate(
+    player1_name = tolower(paste(
+      paste(tail(strsplit(player1, " ")[[1]], -1), collapse = " "),
+      paste0(substr(player1, 1, 1), "."))
+    ),
+    player2_name = tolower(paste(
+      paste(tail(strsplit(player2, " ")[[1]], -1), collapse = " "),
+      paste0(substr(player2, 1, 1), "."))
+    )
+  ) %>%
+  ungroup()
 
-atp_2024_welo <- atp_2024_welo %>%
-  filter(Series == "Grand Slam", Surface == "Grass") 
+### welo 2024 data
 
-names(atp_2024_welo)
+## males
+atp_2024 <- tennis_data("2024", "ATP") # ATP = men's, WTA = women's
+atp_2024_clean <- clean(atp_2024)
+
+atp_2024_welo <- atp_2024_clean %>%
+  filter(Series == "Grand Slam", Tournament == "Wimbledon", Surface == "Grass") 
+
+atp_2024_welo <- welofit(atp_2024_welo)$results
 
 # select only the P_i, P_j, WElo_i_before_match, and WElo_j_before_match columns. Then, 
 # make into only two columns by putting the P_j and corresponding WElo_j_before_match in rows below the 
@@ -114,13 +114,14 @@ atp_2024_welo_avg <- atp_2024_welo_subset %>%
   )
 
 # find number of unique entries in player1 and player2 for subset_2024_m
-unique_players_2024_m <- unique(c(subset_2024_m$player1_name, subset_2024_m$player2_name))
+unique_players_2024_m <- unique(c(subset_2024_m$player1_name, subset_2024_m$player2_name)) 
+length(unique_players_2024_m) # 71
 
-# make P column in atp_2021_welo_avg all lowercase
+# make P column in atp_2024_welo_avg all lowercase
 atp_2024_welo_avg <- atp_2024_welo_avg %>%
   mutate(P = tolower(P))
 
-# merge atp_2021_welo_avg with subset_2024_m to get the average welo for player1 and player2
+# merge atp_2024_welo_avg with subset_2024_m to get the average welo for player1 and player2
 subset_2024_m <- subset_2024_m %>%
   left_join(atp_2024_welo_avg, by = c("player1_name" = "P")) %>%
   rename(player1_avg_welo = avg_welo_before_match) %>%
@@ -138,9 +139,59 @@ subset_2024_m <- subset_2024_m %>%
 
 write.csv(subset_2024_m, "../data/wimbledon_subset_2024_m.csv", row.names = FALSE)
 
-#-----------------------------------------------------------------------------------------------------
+### females
+wta_2024 <- tennis_data("2024", "WTA")
+wta_2024_clean <- clean(wta_2024)
 
-# hard courts
+wta_2024_welo <- wta_2024_clean %>%
+  filter(Tournament == "Wimbledon", Surface == "Grass") 
 
-atp_2024_welo <- atp_2024_welo %>%
-  filter(Series == "Grand Slam", Surface == "Hard") 
+wta_2024_welo <- welofit(wta_2024_welo)$results
+
+# select only the P_i, P_j, WElo_i_before_match, and WElo_j_before_match columns. Then, 
+# make into only two columns by putting the P_j and corresponding WElo_j_before_match in rows below the 
+# P_i nd WElo_i_before_match columns.
+wta_2024_welo_subset <- wta_2024_welo %>%
+  select(P_i, P_j, WElo_i_before_match, WElo_j_before_match) %>%
+  pivot_longer(cols = c(P_i, P_j, WElo_i_before_match, WElo_j_before_match), 
+               names_to = c(".value", "player"), 
+               names_pattern = "(.*)_(i|j)") %>%
+  mutate(player = ifelse(player == "i", "player1", "player2")) %>%
+  select(-player)
+
+# find average welo for each player in atp_2021_welo_subset
+wta_2024_welo_avg <- wta_2024_welo_subset %>%
+  group_by(P) %>%
+  summarise(
+    avg_welo_before_match = mean(WElo, na.rm = TRUE),
+    .groups = 'drop'
+  )
+
+wta_2024_welo_avg <- wta_2024_welo_avg %>%
+  mutate(P = tolower(P))
+
+# find number of unique entries in player1 and player2 for subset_2024_m
+unique_players_2024_f <- unique(c(subset_2024_f$player1_name, subset_2024_f$player2_name)) 
+length(unique_players_2024_f) # 72
+
+# make P column in atp_2021_welo_avg all lowercase
+wta_2024_welo_avg <- wta_2024_welo_avg %>%
+  mutate(P = tolower(P))
+
+# merge atp_2021_welo_avg with subset_2024_m to get the average welo for player1 and player2
+subset_2024_f <- subset_2024_f %>%
+  left_join(wta_2024_welo_avg, by = c("player1_name" = "P")) %>%
+  rename(player1_avg_welo = avg_welo_before_match) %>%
+  left_join(wta_2024_welo_avg, by = c("player2_name" = "P")) %>%
+  rename(player2_avg_welo = avg_welo_before_match)
+
+colSums(is.na(subset_2024_f))
+# make new dataset that contains rows where there are NAs
+subset_2024_f_na <- subset_2024_f %>%
+  filter(is.na(player1_avg_welo) | is.na(player2_avg_welo))
+
+# remove rows in subset_2024_m with NAs
+subset_2024_f <- subset_2024_f %>%
+  filter(!is.na(player1_avg_welo) & !is.na(player2_avg_welo))
+
+write.csv(subset_2024_m, "../data/wimbledon_subset_2024_m.csv", row.names = FALSE)
